931c5023884699a9805fd5fb1bb83170e1425f31
ElinorSel <Elinor.selinder@gmail.com>
2024-04-13 15:55:19 +0200

    ch

---
+++ my-app/src/app/calandar/page.tsx
@@ -0,0 +1,9 @@
+export default function Calandar(){
+    return(
+        <div>
+            <h1>HELLO</h1>
+            <iframe src="https://calendar.google.com/calendar/embed?height=600&wkst=2&ctz=Europe%2FStockholm&bgcolor=%23B39DDB&showNav=0&showDate=0&showPrint=0&showTabs=0&showCalendars=0&showTz=0&src=ZmIzNGYxMjQxNTdmODNkYjNjMDM4ZWU3NTk3MWJmMjZiM2Y0ZjQyYWE0NGMxZDlkNDU3MWFhMjEzOTg1NzNhMkBncm91cC5jYWxlbmRhci5nb29nbGUuY29t&color=%238E24AA" width="800" height="600"  scrolling="no"></iframe>
+        
+        </div>
+         );
+}
