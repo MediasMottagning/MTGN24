3e5bf02476b15d4f48f1e16751b92fa3a2d71262
eddbb <edvin.blomgren@hotmail.com>
2024-07-03 22:44:43 +0200

    groups are separated

--- my-app/src/app/n0llan-grupper/page.tsx
+++ my-app/src/app/n0llan-grupper/page.tsx
@@ -8,83 +8,79 @@ import { UserRecord } from 'firebase-admin/auth';
 
 export default function N0llanGrupper(){
     
-    const [userUrls, setUserUrls] = useState<string[]>([]);
+    const [groupsData, setGroupsData] = useState<string[]>([]);
     const [userData, setUserData] = useState<DocumentData[]>([]);
     const { user } = useAuth();
     const storage = getStorage();
 
     useEffect(() => {
         if (user) {
-            getUserData();
+            getCollectionData();
         }
     }, [user]);
 
-    async function getUserData() {
+    async function getCollectionData() {
         const querySnapshot = await getDocs(collection(db, "users"));
         const usersDataArray = [];
+        //const urls = [];
         for (const userDoc of querySnapshot.docs) {
             const userProfileRef = doc(db, "users", userDoc.id);
             const docSnap = await getDoc(userProfileRef);
             if (docSnap.exists()) {
-                usersDataArray.push(docSnap.data());
-                //console.log(getPicture(docSnap.data().profilePic))
+                var userData = docSnap.data();
+                if (userData.profilePic)
+                    {
+                        try {
+                            const picRef = ref(storage, userData.profilePic);
+                            const url = await getDownloadURL(picRef);
+                            userData.profilePic = url;
+                        } catch (error) {
+                            console.error("Error fetching profile picture:", error);
+                        }
+                    }
+                usersDataArray.push(userData);
             }
         }
+        const groups = usersDataArray
+            .map(obj => obj.group)
+            .filter((group, index, self) => self.indexOf(group) === index)
+        
+        setGroupsData(groups);
         setUserData(usersDataArray);
-    }
-
-    async function getPicture(picUrl: string){
-        try {
-            const picRef = ref(storage, picUrl);
-            const url = await getDownloadURL(picRef);
-            return url;
-        } catch (error) {
-            console.error("Error fetching profile picture:", error);
-        }
+        console.log(usersDataArray)
     }
 
     async function test() {
-        console.log(userData)
+        console.log(groupsData);
     }
-    /*
-    async function getCollectionData() {
-        const urls: string[] = [];
-        const querySnapshot = await getDocs(collection(db, "users"));
-        const storage = getStorage();
 
-        for (const userDoc of querySnapshot.docs) {
-            const userProfileRef = doc(db, "users", userDoc.id);
-            const docSnap = await getDoc(userProfileRef);
+    function groupSeparation(group: string) {
 
-            if (docSnap.exists()) {
-                const userData = docSnap.data();
-                const picUrl = userData.profilePic;
-
-                if (picUrl) {
-                    try {
-                        const picRef = ref(storage, picUrl);
-                        const url = await getDownloadURL(picRef);
-                        urls.push(url);
-                    } catch (error) {
-                        console.error("Error fetching profile picture:", error);
-                    }
-                }
-            }
+        if (group == undefined) {
+            return "some people have not been assinged groups!!"
         }
-        setUserUrls(urls);
+        const groupUsers = userData.filter(user => {if (user.group == group) return user});
+        
+        return(<div key={group + "1"}>
+            <h1>{group}</h1>
+            <div className="grid grid-cols-3 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-6 gap-4">
+                {groupUsers.map((user, index) => (
+                    <div key={index} className="bg-white p-4 rounded-lg shadow-md">
+                        <img src={user.profilePic} alt={`User ${index + 1}`} className="w-full h-auto" />
+                        <h1 className="bg-black">{user.funFact /*n0llan-namn???*/}</h1>
+                    </div>
+                ))}
+            </div>
+        </div>
+        )
     }
-    */
+    
     if (!user){ return <h1>Please login :|</h1>;}
 
     return (
         <main>
-            <div className="grid grid-cols-3 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-6 gap-4">
-                {userUrls.map((url, index) => (
-                    <div key={index} className="bg-red-100 p-4 rounded-lg shadow-md">
-                        <img src={url} alt={`User ${index + 1}`} className="w-full h-auto" />
-                    </div>
-                ))}
-            </div>
+            <h1>n0llan-grupper:</h1>
+            <div>{groupsData.map(groupSeparation)}</div>
             <div>
                 <button onClick={test}>test</button>
             </div>
