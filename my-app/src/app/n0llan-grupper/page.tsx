b059c4250f3f792f5f5d4d025f69ca50b57bdd61
eddbb <edvin.blomgren@hotmail.com>
2024-07-03 23:54:32 +0200

    groups can be expanded

--- my-app/src/app/n0llan-grupper/page.tsx
+++ my-app/src/app/n0llan-grupper/page.tsx
@@ -4,10 +4,11 @@ import useAuth from "../components/useAuth";
 import { doc, getDoc, setDoc, collection, getDocs, DocumentData } from 'firebase/firestore';
 import { db} from '../lib/firebaseConfig';
 import { getStorage, ref, getDownloadURL } from "firebase/storage"; 
-import { UserRecord } from 'firebase-admin/auth';
 
 export default function N0llanGrupper(){
     
+    const [groupBool, setGroupBool] = useState<boolean[]>([]);
+
     const [groupsData, setGroupsData] = useState<string[]>([]);
     const [userData, setUserData] = useState<DocumentData[]>([]);
     const { user } = useAuth();
@@ -22,7 +23,6 @@ export default function N0llanGrupper(){
     async function getCollectionData() {
         const querySnapshot = await getDocs(collection(db, "users"));
         const usersDataArray = [];
-        //const urls = [];
         for (const userDoc of querySnapshot.docs) {
             const userProfileRef = doc(db, "users", userDoc.id);
             const docSnap = await getDoc(userProfileRef);
@@ -47,14 +47,24 @@ export default function N0llanGrupper(){
         
         setGroupsData(groups);
         setUserData(usersDataArray);
-        console.log(usersDataArray)
+        setGroupBool(Array(groups.length).fill(false));
+        
     }
+
+    const toggleGroupBool = (index: number) => {
+        setGroupBool(prevState => {
+            const newState = [...prevState];
+            newState[index] = !newState[index];
+            return newState;
+        });
+    };
 
     async function test() {
         console.log(groupsData);
+        console.log(groupBool)
     }
 
-    function groupSeparation(group: string) {
+    function groupSeparation(group: string, index: number) {
 
         if (group == undefined) {
             return "some people have not been assinged groups!!"
@@ -62,14 +72,17 @@ export default function N0llanGrupper(){
         const groupUsers = userData.filter(user => {if (user.group == group) return user});
         
         return(<div key={group + "1"}>
-            <h1>{group}</h1>
-            <div className="grid grid-cols-3 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-6 gap-4">
-                {groupUsers.map((user, index) => (
-                    <div key={index} className="bg-white p-4 rounded-lg shadow-md">
-                        <img src={user.profilePic} alt={`User ${index + 1}`} className="w-full h-auto" />
-                        <h1 className="bg-black">{user.funFact /*n0llan-namn???*/}</h1>
-                    </div>
-                ))}
+            <button onClick={() => toggleGroupBool(index)} className='bg-black'>{group}</button>
+            <div className={`flexible top-full right-0 mt-2 w-full bg-white shadow-md z-10 flex flex-col items-center gap-4 p-4 transition-opacity duration-300 portrait:flex ${groupBool[index] ? "opacity-100 portrait:block" : "opacity-0 hidden portrait:hidden"}`}>
+                
+                <div className="grid grid-cols-3 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-6 gap-4">
+                    {groupUsers.map((user, index) => (
+                        <div key={index} className="bg-white p-4 rounded-lg shadow-md">
+                            <img src={user.profilePic} alt={`User ${index + 1}`} className="w-full h-auto" />
+                            <h1 className="bg-black">{user.funFact /*n0llan-namn???*/}</h1>
+                        </div>
+                    ))}
+                </div>
             </div>
         </div>
         )
@@ -80,7 +93,7 @@ export default function N0llanGrupper(){
     return (
         <main>
             <h1>n0llan-grupper:</h1>
-            <div>{groupsData.map(groupSeparation)}</div>
+            <div>{groupsData.map((group, index) => groupSeparation(group, index))}</div>
             <div>
                 <button onClick={test}>test</button>
             </div>
