1524d9e83b857f520d646a18a0760561a8e7a2c0
eddbb <81910310+eddbb@users.noreply.github.com>
2024-07-24 22:59:30 +0200

    styling

--- my-app/src/app/n0llan-grupper/page.tsx
+++ my-app/src/app/n0llan-grupper/page.tsx
@@ -87,15 +87,14 @@ export default function N0llanGrupper(){
         }
         const groupUsers = userData.filter(user => {if (user.group == group) return user});
         
-        return(<div key={group + "1"}>
-            <button onClick={() => toggleGroupBool(index)} className='bg-white text-black text-center w-full font-bold'>{group}</button>
-            <div className={`flexible top-full right-0 mt-2 w-full shadow-md z-10 flex flex-col items-center gap-4 p-4 transition-opacity duration-300 portrait:flex ${groupBool[index] ? "opacity-100 portrait:block" : "opacity-0 hidden portrait:hidden"}`}>
-                
-                <div className="grid grid-cols-3 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-6 gap-4">
+        return(<div key={group + "1"} className='flex items-center flex-col'>
+            <button onClick={() => toggleGroupBool(index)} className='bg-white text-black font-bold p-4 mt-4 rounded-lg w-1/3 whitespace-nowrap drop-shadow-lg '>{group}</button>
+            <div className={`flexible top-full right-0 mt-2 w-full z-10 flex flex-col items-center gap-4 p-4 transition-opacity duration-700 portrait:flex ${groupBool[index] ? "opacity-100 portrait:block" : "opacity-0 hidden portrait:hidden"}`}>
+                <div className="grid grid-cols-3 sm:grid-cols-3 md:grid-cols-3 md:w-2/3 lg:grid-cols-5 gap-4 lg:w-2/3">
                     {groupUsers.map((user, index) => (
-                        <button onClick={() => showUserProfile(user.profilePic, user.name, user.funFact)} key={index} className="bg-white p-4 rounded-lg drop-shadow-lg">
-                            <img src={user.profilePic} alt={`User ${index + 1}`} className="w-full h-auto rounded-lg" />
-                            <h1 className="text-black pt-2">{user.name /*n0llan-namn???*/}</h1>
+                        <button onClick={() => showUserProfile(user.profilePic, user.name, user.funFact)} key={index} className="flex flex-col bg-white p-2 rounded-lg drop-shadow-lg items-start">
+                            <img src={user.profilePic} alt={`User ${index + 1}`} className="w-full aspect-square rounded-lg"/>
+                            <h1 className="text-black pt-2 whitespace-nowrap">{user.name}</h1>
                         </button>
                     ))}
                 </div>
@@ -105,19 +104,20 @@ export default function N0llanGrupper(){
     }
     
     if (!user){ return <h1>Please login :|</h1>;}
-
+    //<main className="flex min-h-screen flex-col items-center bg-gradient-to-r from-[#A5CACE] to-[#4FC0A0]">
+    //<div className="flex w-11/12 flex-col mt-5 md:mt-9 max-w-2xl"> {/* EVENT MODULE */}
     return (
-        <main>
+        <main className="flex min-h-screen flex-col bg-gradient-to-r from-[#A5CACE] to-[#4FC0A0]">
             {/*<h1>n0llan-grupper:</h1>*/}
             <div>{groupsData.map((group, index) => groupSeparation(group, index))}</div>
             <div>
                 <button onClick={test}>test</button>
             </div>
-            <div className={`fixed top-0 left-0 w-full h-full flex items-center justify-center z-50 ${popUpBool ? "portrait:block" : "opacity-0 hidden portrait:hidden"}`}>
+            <div className={`fixed top-0 left-0 w-1/4 aspect-square flex items-center justify-center z-50 ${popUpBool ? "portrait:block" : "opacity-0 hidden portrait:hidden"}`}>
                 <div className="bg-white p-8 rounded-lg shadow-lg">
-                    <img src={popUpPic} className="w-auto h-[50vh]"/>
-                    <h1 className="bg-black">{popUpName /*n0llan-namn???*/}</h1>
-                    <h1 className="bg-black">Fun fact: {popUpFunFact}</h1>
+                    <img src={popUpPic} className="w-auto aspect-square"/>
+                    <h1 className="bg-white text-black">{popUpName /*n0llan-namn???*/}</h1>
+                    <h1 className="bg-white text-black">Fun fact: {popUpFunFact}</h1>
                     <button onClick={togglePopUpBool} className="bg-blue-500 text-white px-4 py-2 mt-4 rounded hover:bg-blue-600">
                         Close
                     </button>
