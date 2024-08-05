882ad5edb0159dd6ebba1765af72b7cdeed0f257
fhedma <fhedma@kth.se>
2024-08-05 15:55:28 +0200

    updated Galleri view styling and Header

--- my-app/src/app/components/Header.tsx
+++ my-app/src/app/components/Header.tsx
@@ -17,7 +17,7 @@ export default function Header() {
   };
     return (
         <header className="flex justify-between items-center p-4 relative">
-            <Link href="/home" className="text-2xl font-bold" onClick={closeMenu}>My App</Link>
+            <Link href="/home" className="text-2xl font-bold" onClick={closeMenu}>MTGN24</Link>
             <button className="md:hidden portrait:block" onClick={toggleMenu}>
                 {/* hamburger icon */}
                 <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
@@ -25,19 +25,21 @@ export default function Header() {
                 </svg>
             </button>
             {/* dropdown menu, phone om ni ska lägga in nya länkar lägg in de i bägge menyerna dvs. dropdown och landscape*/}
-            <div className={`absolute top-full right-0 mt-2 w-full bg-white shadow-md z-10 flex flex-col items-center gap-4 p-4 transition-opacity duration-300 portrait:flex ${isMenuOpen ? "opacity-100 portrait:block" : "opacity-0 hidden portrait:hidden"}`}>
+            <div className={`absolute top-full right-0 w-full bg-white shadow-md z-10 flex flex-col items-center gap-4 p-4 transition-opacity duration-300 portrait:flex ${isMenuOpen ? "opacity-100 portrait:block" : "opacity-0 hidden portrait:hidden"}`}>
                 <Link href="/profil" className="py-2 text-blue-500 hover:text-blue-700" onClick={closeMenu}>Profil</Link>
-                <Link href="/event" className="py-2 text-blue-500 hover:text-blue-700" onClick={closeMenu}>Event</Link>
+                <Link href="/event" className="py-2 text-blue-500 hover:text-blue-700" onClick={closeMenu}>Galleri</Link>
                 <Link href="/n0llan-grupper" className="py-2 text-blue-500 hover:text-blue-700" onClick={closeMenu}>n0llan</Link>
                 <Link href="/phosar-grupper" className="py-2 text-blue-500 hover:text-blue-700" onClick={closeMenu}>Phösare</Link>
+                <Link href="/calandar" className="text-blue-500 hover:text-blue-700" onClick={closeMenu}>Calendar</Link>
                 <LogoutButton onClose={closeMenu} />
             </div>
             {/* landscape menu, computer*/}
             <div className="hidden landscape:flex items-center gap-4">
                 <Link href="/profil" className="text-blue-500 hover:text-blue-700">Profil</Link>
-                <Link href="/event" className="text-blue-500 hover:text-blue-700">Event</Link>
+                <Link href="/event" className="text-blue-500 hover:text-blue-700">Galleri</Link>
                 <Link href="/n0llan-grupper" className="text-blue-500 hover:text-blue-700">n0llan</Link>
                 <Link href="/phosar-grupper" className="text-blue-500 hover:text-blue-700">Phösare</Link>
+                <Link href="/calandar" className="text-blue-500 hover:text-blue-700">Calendar</Link>
                 <LogoutButton />
             </div>
         </header>
