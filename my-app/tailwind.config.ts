14e6c7bbd8e90ae92c985fbfe48aa793b1761aca
linus <linusbalt3r@gmail.com>
2024-08-10 16:37:30 +0200

    Added basic CSS.

--- my-app/tailwind.config.ts
+++ my-app/tailwind.config.ts
@@ -26,10 +26,21 @@ const config: Config = {
           '0%': { opacity: "0" },
           '100%': { opacity: "1" },
         },
+        fadeInFromBottom: {
+          '0%': { transform: 'translateY(100%)', opacity: '0' }, // Start from bottom and invisible
+          '100%': { transform: 'translateY(0)', opacity: '1' },  // End in original position and fully visible
+        },
+        pulse: {
+          '0%, 100%': { opacity: "1" },
+          '50%': { opacity: "0.7" },
+        },
       },
       animation: {
         fadeIn: 'fadeIn 0.4s ease-in-out',
+        fadeInFromBottom: 'fadeInFromBottom 0.5s ease-out forwards', // Customize duration and easing
+        pulseInfinite: 'pulse 4s cubic-bezier(0, 0, 1, 1) infinite',
       },
+
     },
   },
   plugins: [],
