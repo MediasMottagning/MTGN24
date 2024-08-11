91c4e06a0416b30069f44bc40055aa37e4aa51ff
linus <linusbalt3r@gmail.com>
2024-08-11 20:02:26 +0200

    Now only renders navigation links if user is logged in.

--- my-app/src/app/components/Header.tsx
+++ my-app/src/app/components/Header.tsx
@@ -26,50 +26,54 @@ export default function Header() {
         <Image src="/logo.png" alt="MTGN24" width={32} height={32} className='mr-2 ml-4'/>
         MTGN24
       </Link>
-      <button className="md:hidden portrait:block" onClick={toggleMenu}>
-        {/* hamburger icon */}
-        <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-          <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6h16M4 12h16m-7 6h7" />
-        </svg>
-      </button>
-      {/* dropdown menu */}
-      <div className={`absolute top-full right-0 w-full bg-white shadow-md z-10`}>
-        <div className={`transition-all delay-150 duration-200 overflow-hidden w-full ${isMenuOpen ? "max-h-[20rem]" : "max-h-0"}`}>
-          <div className="flex flex-col items-stretch">
-            <div className="w-full px-4 py-2 hover:bg-gray-200">
-              <Link href="/profil" className="block text-center" onClick={closeMenu}>Profil</Link>
-            </div>
-            <div className="w-full px-4 py-2 hover:bg-gray-200">
-              <Link href="/event" className="block text-center" onClick={closeMenu}>Galleri</Link>
-            </div>
-            <div className="w-full px-4 py-2 hover:bg-gray-200">
-              <Link href="/n0llan" className="block text-center" onClick={closeMenu}>nØllan</Link>
-            </div>
-            <div className="w-full px-4 py-2 hover:bg-gray-200">
-              <Link href="/phosare" className="block text-center" onClick={closeMenu}>Phösare</Link>
-            </div>
-            <div className="w-full px-4 py-2 hover:bg-gray-200">
-              <Link href="/blandaren" className="block text-center" onClick={closeMenu}>Bländare</Link>
-            </div>
-            <div className="w-full px-4 py-2 hover:bg-gray-200">
-              <Link href="/calandar" className="block text-center" onClick={closeMenu}>Calendar</Link>
-            </div>
-            <div className="w-full px-4 py-2 hover:bg-gray-200">
-              <LogoutButton onClose={closeMenu} />
+      {user && (
+        <>
+          <button className="md:hidden portrait:block" onClick={toggleMenu}>
+          {/* hamburger icon */}
+          <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6h16M4 12h16m-7 6h7" />
+          </svg>
+        </button>
+        {/* dropdown menu */}
+        <div className={`absolute top-full right-0 w-full bg-white shadow-md z-10`}>
+          <div className={`transition-all delay-150 duration-200 overflow-hidden w-full ${isMenuOpen ? "max-h-[20rem]" : "max-h-0"}`}>
+            <div className="flex flex-col items-stretch">
+              <div className="w-full px-4 py-2 hover:bg-gray-200">
+                <Link href="/profil" className="block text-center" onClick={closeMenu}>Profil</Link>
+              </div>
+              <div className="w-full px-4 py-2 hover:bg-gray-200">
+                <Link href="/event" className="block text-center" onClick={closeMenu}>Galleri</Link>
+              </div>
+              <div className="w-full px-4 py-2 hover:bg-gray-200">
+                <Link href="/n0llan" className="block text-center" onClick={closeMenu}>nØllan</Link>
+              </div>
+              <div className="w-full px-4 py-2 hover:bg-gray-200">
+                <Link href="/phosare" className="block text-center" onClick={closeMenu}>Phösare</Link>
+              </div>
+              <div className="w-full px-4 py-2 hover:bg-gray-200">
+                <Link href="/blandaren" className="block text-center" onClick={closeMenu}>Bländare</Link>
+              </div>
+              <div className="w-full px-4 py-2 hover:bg-gray-200">
+                <Link href="/calandar" className="block text-center" onClick={closeMenu}>Calendar</Link>
+              </div>
+              <div className="w-full px-4 py-2 hover:bg-gray-200">
+                <LogoutButton onClose={closeMenu} />
+              </div>
             </div>
           </div>
         </div>
-      </div>
-      {/* landscape menu */}
-      <div className="hidden landscape:flex items-center gap-4">
-        <Link href="/profil" className="text-gray-700 hover:text-blue-400 font-medium transition ease-in-out">Profil</Link>
-        <Link href="/event" className="text-gray-700 hover:text-blue-400 font-medium transition ease-in-out">Galleri</Link>
-        <Link href="/n0llan" className="text-gray-700 hover:text-blue-400 font-medium transition ease-in-out">nØllan</Link>
-        <Link href="/phosare" className="text-gray-700 hover:text-blue-400 font-medium transition ease-in-out">Phösare</Link>
-        <Link href="/blandaren" className="text-gray-700 hover:text-blue-400 font-medium transition ease-in-out">Bländare</Link>
-        <Link href="/calandar" className="text-gray-700 hover:text-blue-400 font-medium transition ease-in-out">Kalender</Link>
-        <LogoutButton/>
-      </div>
+        {/* landscape menu */}
+        <div className="hidden landscape:flex items-center gap-4">
+          <Link href="/profil" className="text-gray-700 hover:text-blue-400 font-medium transition ease-in-out">Profil</Link>
+          <Link href="/event" className="text-gray-700 hover:text-blue-400 font-medium transition ease-in-out">Galleri</Link>
+          <Link href="/n0llan" className="text-gray-700 hover:text-blue-400 font-medium transition ease-in-out">nØllan</Link>
+          <Link href="/phosare" className="text-gray-700 hover:text-blue-400 font-medium transition ease-in-out">Phösare</Link>
+          <Link href="/blandaren" className="text-gray-700 hover:text-blue-400 font-medium transition ease-in-out">Bländare</Link>
+          <Link href="/calandar" className="text-gray-700 hover:text-blue-400 font-medium transition ease-in-out">Kalender</Link>
+          <LogoutButton/>
+        </div>
+        </>
+      )}
     </header>
   );
 }
