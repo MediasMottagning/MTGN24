f33b19a7ebd91f89ec735166c6110c391e268859
linus <linusbalt3r@gmail.com>
2024-08-12 17:40:48 +0200

    Added min height to component and nowrap to title.

--- my-app/src/app/components/EventCard.tsx
+++ my-app/src/app/components/EventCard.tsx
@@ -13,19 +13,18 @@ const backupImage = "/backup.webp";
 
 
 export default function EventCard(params:any) {
-
     
     function handleQuestionMark() {
         params.onModalClick();
     }
 
     return (
-        <div className="flex min-w-[300px] w-full flex-row h-36 sm:h-48 bg-white drop-shadow-homeShadow rounded-lg animate-fadeIn">
+        <div className="flex min-w-[300px] min-h-[70px] w-full flex-row h-36 sm:h-48 bg-white drop-shadow-homeShadow rounded-lg animate-fadeIn">
             <div className="flex w-1/3 sm:w-2/5 relative mr-1">
                 <Image alt="Preview image of event" src={params.image || backupImage} fill={true} objectFit="cover" className="rounded-l-md"></Image>
             </div>
             <div className="flex w-2/3 sm:w-3/5 flex-col place-content-between p-2 sm:p-3">
-                <p className="font-semibold sm:text-xl">{params.title || "Event title"}</p>
+                <p className="font-semibold sm:text-xl whitespace-nowrap overflow-hidden text-ellipsis">{params.title || "Event title"}</p>
                 
                 <div className="flex flex-row items-center">
                     <div className="mr-1 w-6 sm:w-7">
