e720b835f56c6bf281e79213286f50a5f8b18134
linus <linusbalt3r@gmail.com>
2024-08-12 21:52:42 +0200

    Added white text to mimic actual home page.

--- my-app/src/app/components/TemplateHome.tsx
+++ my-app/src/app/components/TemplateHome.tsx
@@ -8,7 +8,7 @@ export default function Home() {
         <main className="flex min-h-screen min-w-72 flex-col items-center bg-gradient-to-r from-[#A5CACE] to-[#4FC0A0]">
             <div className="flex w-11/12 flex-col mt-5 md:mt-9 max-w-2xl"> {/* EVENT MODULE */}
                 <div className="flex flex-row">
-                    <p className="font-semibold text-lg sm:text-2xl ml-1">Nästa event</p>
+                    <p className="font-semibold text-lg text-white drop-shadow-xl sm:text-2xl ml-1">Nästa event</p>
                     <p className="text-2xl sm:text-3xl ml-1">🥳</p>
                 </div>
                 <div className="flex flex-col space-y-5">
@@ -32,7 +32,7 @@ export default function Home() {
             </div>
             <div className="flex w-11/12 flex-col mt-5 md:mt-9 max-w-2xl mb-5"> {/* EVENT MODULE */}
             <div className="flex flex-row">
-                    <p className="font-semibold text-lg sm:text-2xl pl-1">Senaste anslag</p>
+                    <p className="font-semibold text-lg text-white drop-shadow-xl sm:text-2xl pl-1">Senaste anslag</p>
                     <p className="text-2xl sm:text-3xl ml-1">📣</p>
                 </div>
                 
