8aad26f64b18a8f7eb03bc9e7430260682ca0934
ElinorSel <Elinor.selinder@gmail.com>
2024-08-19 15:48:54 +0200

    Namn Quiz
    yuuuh hope it doesnt break

--- my-app/src/app/components/Header.tsx
+++ my-app/src/app/components/Header.tsx
@@ -5,15 +5,14 @@ import Link from "next/link";
 import Image from "next/image";
 import useAuth from "./useAuth";
 
-
-export default function Header() {    
+export default function Header() {
   const { user } = useAuth();
 
   // toggle dropdown menu visibility
   const toggleMenu = () => {
     setIsMenuOpen(!isMenuOpen);
   };
-    const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
 
   // close dropdown menu when a link is clicked
   const closeMenu = () => {
@@ -21,61 +20,165 @@ export default function Header() {
   };
 
   return (
-    <header className="flex justify-between items-center p-4 relative">          
-      <Link href="/home" className="flex items-center text-2xl font-bold" onClick={closeMenu}>            
-        <Image src="/logo.png" alt="MTGN24" width={32} height={32} className='mr-2 ml-4'/>
+    <header className="flex justify-between items-center p-4 relative">
+      <Link
+        href="/home"
+        className="flex items-center text-2xl font-bold"
+        onClick={closeMenu}
+      >
+        <Image
+          src="/logo.png"
+          alt="MTGN24"
+          width={32}
+          height={32}
+          className="mr-2 ml-4"
+        />
         MTGN24
       </Link>
       {user && (
         <>
           <button className="md:hidden portrait:block" onClick={toggleMenu}>
-          {/* hamburger icon */}
-          <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6h16M4 12h16m-7 6h7" />
-          </svg>
-        </button>
-        {/* dropdown menu */}
-        <div className={`absolute top-full right-0 w-full bg-white shadow-md z-10`}>
-          <div className={`transition-all delay-150 duration-200 overflow-hidden w-full ${isMenuOpen ? "max-h-[22rem]" : "max-h-0"}`}>
-            <div className="flex flex-col">
-              <div className="w-full px-4 py-2 hover:bg-gray-200">
-                <Link href="/profil" className="block text-center" onClick={closeMenu}>Profil</Link>
-              </div>
-              <div className="w-full px-4 py-2 hover:bg-gray-200">
-                <Link href="/event" className="block text-center" onClick={closeMenu}>Galleri</Link>
-              </div>
-              <div className="w-full px-4 py-2 hover:bg-gray-200">
-                <Link href="/n0llan" className="block text-center" onClick={closeMenu}>nØllan</Link>
-              </div>
-              <div className="w-full px-4 py-2 hover:bg-gray-200">
-                <Link href="/phosare" className="block text-center" onClick={closeMenu}>Phösare</Link>
-              </div>
-              <div className="w-full px-4 py-2 hover:bg-gray-200">
-                <Link href="/blandaren" className="block text-center" onClick={closeMenu}>Bländare</Link>
-              </div>
-              <div className="w-full px-4 py-2 hover:bg-gray-200">
-                <Link href="/calandar" className="block text-center" onClick={closeMenu}>Calendar</Link>
-              </div>
-              <div className="w-full px-4 py-2 hover:bg-gray-200">
-                <Link href="https://forms.gle/zDnjmj2kjuQdgNFd9" className="block text-center">På hjärtat-lådan ❤️</Link>
-              </div>
-              <div className="flex flex-col w-full px-4 py-2 mt-2 hover:bg-gray-200">
-                <LogoutButton onClose={closeMenu} />
+            {/* hamburger icon */}
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-6 w-6"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth={2}
+                d="M4 6h16M4 12h16m-7 6h7"
+              />
+            </svg>
+          </button>
+          {/* dropdown menu */}
+          <div
+            className={`absolute top-full right-0 w-full bg-white shadow-md z-10`}
+          >
+            <div
+              className={`transition-all delay-150 duration-200 overflow-hidden w-full ${
+                isMenuOpen ? "max-h-[22rem]" : "max-h-0"
+              }`}
+            >
+              <div className="flex flex-col">
+                <div className="w-full px-4 py-2 hover:bg-gray-200">
+                  <Link
+                    href="/profil"
+                    className="block text-center"
+                    onClick={closeMenu}
+                  >
+                    Profil
+                  </Link>
+                </div>
+                <div className="w-full px-4 py-2 hover:bg-gray-200">
+                  <Link
+                    href="/event"
+                    className="block text-center"
+                    onClick={closeMenu}
+                  >
+                    Galleri
+                  </Link>
+                </div>
+                <div className="w-full px-4 py-2 hover:bg-gray-200">
+                  <Link
+                    href="/n0llan"
+                    className="block text-center"
+                    onClick={closeMenu}
+                  >
+                    nØllan
+                  </Link>
+                </div>
+                <div className="w-full px-4 py-2 hover:bg-gray-200">
+                  <Link
+                    href="/phosare"
+                    className="block text-center"
+                    onClick={closeMenu}
+                  >
+                    Phösare
+                  </Link>
+                </div>
+                <div className="w-full px-4 py-2 hover:bg-gray-200">
+                  <Link
+                    href="/blandaren"
+                    className="block text-center"
+                    onClick={closeMenu}
+                  >
+                    Bländare
+                  </Link>
+                </div>
+                <div className="w-full px-4 py-2 hover:bg-gray-200">
+                  <Link
+                    href="/calandar"
+                    className="block text-center"
+                    onClick={closeMenu}
+                  >
+                    Calendar
+                  </Link>
+                </div>
+                <div className="w-full px-4 py-2 hover:bg-gray-200">
+                  <Link
+                    href="/quiz"
+                    className="block text-center"
+                    onClick={closeMenu}
+                  >
+                    Namn Quiz
+                  </Link>
+                </div>
+                <div className="flex flex-col w-full px-4 py-2 mt-2 hover:bg-gray-200">
+                  <LogoutButton onClose={closeMenu} />
+                </div>
               </div>
             </div>
           </div>
-        </div>
-        {/* landscape menu */}
-        <div className="hidden landscape:flex items-center gap-4">
-          <Link href="/profil" className="text-gray-700 hover:text-blue-400 font-medium transition ease-in-out">Profil</Link>
-          <Link href="/event" className="text-gray-700 hover:text-blue-400 font-medium transition ease-in-out">Galleri</Link>
-          <Link href="/n0llan" className="text-gray-700 hover:text-blue-400 font-medium transition ease-in-out">nØllan</Link>
-          <Link href="/phosare" className="text-gray-700 hover:text-blue-400 font-medium transition ease-in-out">Phösare</Link>
-          <Link href="/blandaren" className="text-gray-700 hover:text-blue-400 font-medium transition ease-in-out">Bländare</Link>
-          <Link href="/calandar" className="text-gray-700 hover:text-blue-400 font-medium transition ease-in-out">Kalender</Link>
-          <Link href="https://forms.gle/zDnjmj2kjuQdgNFd9" className="text-gray-700 hover:text-blue-400 font-medium transition ease-in-out">På hjärtat-lådan ❤️ 📫</Link>
-          <LogoutButton/>
-        </div>
+          {/* landscape menu */}
+          <div className="hidden landscape:flex items-center gap-4">
+            <Link
+              href="/profil"
+              className="text-gray-700 hover:text-blue-400 font-medium transition ease-in-out"
+            >
+              Profil
+            </Link>
+            <Link
+              href="/event"
+              className="text-gray-700 hover:text-blue-400 font-medium transition ease-in-out"
+            >
+              Galleri
+            </Link>
+            <Link
+              href="/n0llan"
+              className="text-gray-700 hover:text-blue-400 font-medium transition ease-in-out"
+            >
+              nØllan
+            </Link>
+            <Link
+              href="/phosare"
+              className="text-gray-700 hover:text-blue-400 font-medium transition ease-in-out"
+            >
+              Phösare
+            </Link>
+            <Link
+              href="/blandaren"
+              className="text-gray-700 hover:text-blue-400 font-medium transition ease-in-out"
+            >
+              Bländare
+            </Link>
+            <Link
+              href="/calandar"
+              className="text-gray-700 hover:text-blue-400 font-medium transition ease-in-out"
+            >
+              Kalender
+            </Link>
+            <Link
+              href="/quiz"
+              className="text-gray-700 hover:text-blue-400 font-medium transition ease-in-out"
+            >
+              Namn Quiz
+            </Link>
+            <LogoutButton />
+          </div>
         </>
       )}
     </header>
--- my-app/src/app/n0llan/page.tsx
+++ my-app/src/app/n0llan/page.tsx
@@ -1,149 +1,251 @@
-'use client'
-import { FormEvent, useEffect, useState } from 'react';
+"use client";
+import { FormEvent, useEffect, useState } from "react";
 import useAuth from "../components/useAuth";
-import { doc, getDoc, setDoc, collection, getDocs, DocumentData } from 'firebase/firestore';
-import { db } from '../lib/firebaseConfig';
+import {
+  doc,
+  getDoc,
+  setDoc,
+  collection,
+  getDocs,
+  DocumentData,
+} from "firebase/firestore";
+import { db } from "../lib/firebaseConfig";
 import { getStorage, ref, getDownloadURL } from "firebase/storage";
 
 export default function N0llanGrupper() {
+  const [groupBool, setGroupBool] = useState<boolean[]>([]);
+  const [groupsData, setGroupsData] = useState<string[]>([]);
+  const [userData, setUserData] = useState<DocumentData[]>([]);
 
-    const [groupBool, setGroupBool] = useState<boolean[]>([]);
-    const [groupsData, setGroupsData] = useState<string[]>([]);
-    const [userData, setUserData] = useState<DocumentData[]>([]);
+  const [popUpBool, setPopUpBool] = useState(false);
+  const [popUpName, setPopUpName] = useState("");
+  const [popUpPic, setPopUpPic] = useState("");
+  const [popUpFunFact, setPopUpFunFact] = useState("");
 
-    const [popUpBool, setPopUpBool] = useState(false);
-    const [popUpName, setPopUpName] = useState("");
-    const [popUpPic, setPopUpPic] = useState("");
-    const [popUpFunFact, setPopUpFunFact] = useState("");
+  const { user } = useAuth();
+  const storage = getStorage();
 
-    const { user } = useAuth();
-    const storage = getStorage();
-
- 
-    /* CODE FOR FETCHTING n0llan */
-    const [users, setUsers] = useState<{ group: string }[]>([]);
-    useEffect(() => {
-        const fetchUsers = async () => {
-            if (!user){ return <h1>Please login</h1>;} // If middleware.ts is working this should never be rendered
-            const token = await user.getIdToken();
-            try {
-                const response = await fetch('/api/getUsers', {
-                    method: 'GET',
-                    headers: {
-                        'Authorization': `Bearer ${token}`
-                    }
-                });
-                if (response.ok) {
-                    const data = await response.json();
-                    //console.log('Users:', data);
-                    setUsers(data);
-                } else {
-                    console.error('Failed to fetch users');
-                }
-            } catch (error) {
-                console.error('Error fetching users:', error);
-            }
-        };
-        fetchUsers();
-    }, [user]);
-
-    /* GROUPING n0llan */
-    useEffect(() => {
-        if (users.length > 0) {
-            const groups = users
-                .map(obj => obj.group)
-                .filter((group, index, self) => self.indexOf(group) === index);
-
-            setGroupsData(groups);
-            setUserData(users);
-            setGroupBool(Array(groups.length).fill(false));
-        }
-    }, [users]);
-
-
-    const toggleGroupBool = (index: number) => {
-        setGroupBool(prevState => {
-            const newState = [...prevState];
-            newState[index] = !newState[index];
-            return newState;
+  /* CODE FOR FETCHTING n0llan */
+  const [users, setUsers] = useState<{ group: string }[]>([]);
+  useEffect(() => {
+    const fetchUsers = async () => {
+      if (!user) {
+        return <h1>Please login</h1>;
+      } // If middleware.ts is working this should never be rendered
+      const token = await user.getIdToken();
+      try {
+        const response = await fetch("/api/getUsers", {
+          method: "GET",
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
         });
-    };
-
-    const togglePopUpBool = () => {
-        setPopUpBool(!popUpBool);
-    };
-
-    async function showUserProfile(profilePic: string, name: string, funFact: string) {
-        togglePopUpBool();
-        setPopUpPic(profilePic);
-        setPopUpName(name);
-        setPopUpFunFact(funFact);
-    }
-
-    function groupSeparation(group: string, index: number) {
-
-        if (group == undefined) {
-            return 
+        if (response.ok) {
+          const data = await response.json();
+          //console.log('Users:', data);
+          setUsers(data);
+        } else {
+          console.error("Failed to fetch users");
         }
-        const groupUsers = userData.filter(user => { if (user.group == group && !user.phosGroup) return user });
-        const phosUsers = userData.filter(user => { if (user.group == group && user.phosGroup != "KPH" && user.phosGroup) return user });
-        const kphUsers = userData.filter(user => { if (user.group == group && user.phosGroup == "KPH") return user });
+      } catch (error) {
+        console.error("Error fetching users:", error);
+      }
+    };
+    fetchUsers();
+  }, [user]);
 
-        return (<div key={group + "1"} className='flex items-center flex-col mx-7 sm:mx-16 md:mx-32 lg:mx-64 xl:mx-96'>
-            <button onClick={() => toggleGroupBool(index)} className='bg-white text-black font-normal text-xl mt-4 rounded-lg w-full py-4 whitespace-nowrap drop-shadow hover:bg-slate-200'>{group}
-                <div className='text-right pr-3 pb-3 h-2'>
-                    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
-                    {groupBool[index] ? <i className="material-symbols-outlined"></i> : <i className="material-symbols-outlined"></i>}
-                </div>
-            </button>
-            <div className={`transition-all delay-150 duration-200 overflow-hidden w-full ${groupBool[index] ? "max-h-[200rem]" : "max-h-0"}`}> {/* KANSKE MÅSTE ÄNDRA VÄRDE PÅ max-h- beroende på hur många som kommer visas upp i animationen */}
-                <h1 className="text-black whitespace-nowrap text-center text-lg bg-white my-5 py-1 drop-shadow rounded-lg">NØllan</h1>
-                <div className="grid grid-cols-3 gap-4 lg:grid-cols-4 2xl:grid-cols-5">
-                    {groupUsers.map((user, index) => (
-                        <button onClick={() => showUserProfile(user.profilePic, user.name, user.funFact)} key={index} className="bg-white p-2 rounded-lg drop-shadow hover:bg-slate-200">
-                            <img src={user.profilePic} alt={`User ${index + 1}`} className="w-full aspect-square rounded-lg" />
-                            <h1 className="text-black text-xs pt-2 whitespace-nowrap">{user.name}</h1>
-                        </button>
-                    ))}
+  /* GROUPING n0llan */
+  useEffect(() => {
+    if (users.length > 0) {
+      const groups = users
+        .map((obj) => obj.group)
+        .filter((group, index, self) => self.indexOf(group) === index);
 
-                </div>
-                <h1 className="text-black whitespace-nowrap text-center text-lg bg-white my-5 py-1 drop-shadow rounded-lg">Bästisar</h1>
-                <div className="grid grid-cols-2 gap-4 mb-3 sm:mx-20 2xl:mx-64">
-                    {kphUsers.map((user, index) => (
-                        <button onClick={() => showUserProfile(user.profilePic, user.name, user.funFact)} key={index} className="bg-white p-2 rounded-lg drop-shadow hover:bg-slate-200">
-                            <img src={user.profilePic} alt={user.name} className="w-full aspect-square rounded-lg" />
-                            <h1 className="text-black text-xs pt-2 whitespace-nowrap">{user.name}</h1>
-                        </button>
-                    ))}
-                </div>
-                <div className="grid grid-cols-3 gap-4 2xl:mx-48">
-                    {phosUsers.map((user, index) => (
-                        <button onClick={() => showUserProfile(user.profilePic, user.name, user.funFact)} key={index} className="bg-white p-2 rounded-lg drop-shadow hover:bg-slate-200">
-                            <img src={user.profilePic} alt={`User ${index + 1}`} className="w-full aspect-square rounded-lg" />
-                            <h1 className="text-black text-xs pt-2 whitespace-nowrap">{user.name}</h1>
-                        </button>
-                    ))}
-                </div>
-                <div className='mb-10'></div>
-            </div>
-        </div>
-        )
+      setGroupsData(groups);
+      setUserData(users);
+      setGroupBool(Array(groups.length).fill(false));
     }
+  }, [users]);
 
-    if (!user) { return <h1>Please login :|</h1>; } // If middleware.ts is working this should never be rendered
+  const toggleGroupBool = (index: number) => {
+    setGroupBool((prevState) => {
+      const newState = [...prevState];
+      newState[index] = !newState[index];
+      return newState;
+    });
+  };
+
+  const togglePopUpBool = () => {
+    setPopUpBool(!popUpBool);
+  };
+
+  async function showUserProfile(
+    profilePic: string,
+    name: string,
+    funFact: string
+  ) {
+    togglePopUpBool();
+    setPopUpPic(profilePic);
+    setPopUpName(name);
+    setPopUpFunFact(funFact);
+  }
+
+  function groupSeparation(group: string, index: number) {
+    if (group == undefined) {
+      return;
+    }
+    const groupUsers = userData.filter((user) => {
+      if (user.group == group && !user.phosGroup) return user;
+    });
+    const phosUsers = userData.filter((user) => {
+      if (user.group == group && user.phosGroup != "KPH" && user.phosGroup)
+        return user;
+    });
+    const kphUsers = userData.filter((user) => {
+      if (user.group == group && user.phosGroup == "KPH") return user;
+    });
+
     return (
-        <main className="min-h-screen bg-gradient-to-r from-[#A5CACE] to-[#4FC0A0]">
-            <div>{groupsData.map((group, index) => groupSeparation(group, index))}</div>
-            <div onClick={togglePopUpBool} className='flex items-center justify-center '>
-                <div className={`fixed aspect-square text-center top-20 h-1/3 sm:h-2/5 drop-shadow  ${popUpBool ? "" : "opacity-0 hidden"}`}>
-                    <div className="bg-white p-8 rounded-lg shadow-lg hover:bg-slate-200">
-                        <img src={popUpPic} className="w-full aspect-square rounded-lg" />
-                        <h1 className="text-black text-xl font-bold p-1">{popUpName}</h1>
-                        <h1 className="text-black">Fun fact: {popUpFunFact}</h1>
-                    </div>
-                </div>
-            </div>
+      <div
+        key={group + "1"}
+        className="flex items-center flex-col mx-7 sm:mx-16 md:mx-32 lg:mx-64 xl:mx-96"
+      >
+        <button
+          onClick={() => toggleGroupBool(index)}
+          className="bg-white text-black font-normal text-xl mt-4 rounded-lg w-full py-4 whitespace-nowrap drop-shadow backdrop-blur-sm bg-no-repeat bg-cover hover:bg-slate-200"
+          style={{
+            backgroundImage:
+              group == "Wall-E"
+                ? "url(https://i.imgur.com/BGFMVnH.png)"
+                : group == "Svampbobbarna"
+                ? "url(https://i.imgur.com/aAKn374.png)"
+                : group == "Flower Power"
+                ? "url(https://i.imgur.com/G8xZ7OU.png)"
+                : group === "Bulbasaurs"
+                ? "url(https://i.imgur.com/01crBHP.png)"
+                : group === "Amazonas"
+                ? "url(https://i.imgur.com/cpXydCG.png)"
+                : group === "Plantpatrullen"
+                ? "url(https://i.imgur.com/Tbb7gWD.png)"
+                : "none",
+          }}
+        >
+          {group}
+          <div className="text-right pr-3 pb-3 h-2">
+            <link
+              rel="stylesheet"
+              href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200"
+            />
+            {groupBool[index] ? (
+              <i className="material-symbols-outlined"></i>
+            ) : (
+              <i className="material-symbols-outlined"></i>
+            )}
+          </div>
+        </button>
+        <div
+          className={`transition-all delay-150 duration-200 overflow-hidden w-full ${
+            groupBool[index] ? "max-h-[200rem]" : "max-h-0"
+          }`}
+        >
+          {" "}
+          {/* KANSKE MÅSTE ÄNDRA VÄRDE PÅ max-h- beroende på hur många som kommer visas upp i animationen */}
+          <h1 className="text-black whitespace-nowrap text-center text-lg bg-white my-5 py-1 drop-shadow rounded-lg">
+            NØllan
+          </h1>
+          <div className="grid grid-cols-3 gap-4 lg:grid-cols-4 2xl:grid-cols-5">
+            {groupUsers.map((user, index) => (
+              <button
+                onClick={() =>
+                  showUserProfile(user.profilePic, user.name, user.funFact)
+                }
+                key={index}
+                className="bg-white p-2 rounded-lg drop-shadow hover:bg-slate-200"
+              >
+                <img
+                  src={user.profilePic}
+                  alt={`User ${index + 1}`}
+                  className="w-full aspect-square rounded-lg"
+                />
+                <h1 className="text-black text-xs pt-2 whitespace-nowrap">
+                  {user.name}
+                </h1>
+              </button>
+            ))}
+          </div>
+          <h1 className="text-black whitespace-nowrap text-center text-lg bg-white my-5 py-1 drop-shadow rounded-lg">
+            Bästisar
+          </h1>
+          <div className="grid grid-cols-2 gap-4 mb-3 sm:mx-20 2xl:mx-64">
+            {kphUsers.map((user, index) => (
+              <button
+                onClick={() =>
+                  showUserProfile(user.profilePic, user.name, user.funFact)
+                }
+                key={index}
+                className="bg-white p-2 rounded-lg drop-shadow hover:bg-slate-200"
+              >
+                <img
+                  src={user.profilePic}
+                  alt={user.name}
+                  className="w-full aspect-square rounded-lg"
+                />
+                <h1 className="text-black text-xs pt-2 whitespace-nowrap">
+                  {user.name}
+                </h1>
+              </button>
+            ))}
+          </div>
+          <div className="grid grid-cols-3 gap-4 2xl:mx-48">
+            {phosUsers.map((user, index) => (
+              <button
+                onClick={() =>
+                  showUserProfile(user.profilePic, user.name, user.funFact)
+                }
+                key={index}
+                className="bg-white p-2 rounded-lg drop-shadow hover:bg-slate-200"
+              >
+                <img
+                  src={user.profilePic}
+                  alt={`User ${index + 1}`}
+                  className="w-full aspect-square rounded-lg"
+                />
+                <h1 className="text-black text-xs pt-2 whitespace-nowrap">
+                  {user.name}
+                </h1>
+              </button>
+            ))}
+          </div>
+          <div className="mb-10"></div>
+        </div>
+      </div>
+    );
+  }
 
-        </main>
-    )
+  if (!user) {
+    return <h1>Please login :|</h1>;
+  } // If middleware.ts is working this should never be rendered
+  return (
+    <main className="min-h-screen bg-gradient-to-r from-[#A5CACE] to-[#4FC0A0]">
+      <div>
+        {groupsData.map((group, index) => groupSeparation(group, index))}
+      </div>
+      <div
+        onClick={togglePopUpBool}
+        className="flex items-center justify-center "
+      >
+        <div
+          className={`fixed aspect-square text-center top-20 h-1/3 sm:h-2/5 drop-shadow  ${
+            popUpBool ? "" : "opacity-0 hidden"
+          }`}
+        >
+          <div className="bg-white p-8 rounded-lg shadow-lg hover:bg-slate-200">
+            <img src={popUpPic} className="w-full aspect-square rounded-lg" />
+            <h1 className="text-black text-xl font-bold p-1">{popUpName}</h1>
+            <h1 className="text-black">Fun fact: {popUpFunFact}</h1>
+          </div>
+        </div>
+      </div>
+    </main>
+  );
 }
